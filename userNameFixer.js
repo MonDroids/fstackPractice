0158cadeb69ec8f5b28168a9e56c483bf7b7034e
SainDev <46877765+MonDroids@users.noreply.github.com>
2025-07-03 21:01:23 +0800

    UpperCaseLowerCaseFixerOfUser
    testetstest

---
+++ userNameFixer.js
@@ -0,0 +1,9 @@
+var name = prompt("What is your name: ");
+var firstChar = name.slice(0,1);
+
+var upperCase = firstChar.toUpperCase();
+
+var restOfName = name.slice(1, name.length);
+restOfName = restOfName.toLowerCase();
+var capitalisedName = upperCase + restOfName;
+alert("hello " + capitalisedName);
